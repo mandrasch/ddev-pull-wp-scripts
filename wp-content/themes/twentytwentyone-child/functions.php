b7cf0f99e721539c06f800c23120249e93bb4978
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-01-27 08:19:23 +0100

    add better child theme example

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -4,6 +4,7 @@
 function child_theme_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() . '/assets/css/page.css', array('parent-style'));
+    wp_enqueue_style('child-theme-css-custom', get_stylesheet_directory_uri() . '/assets/css/custom.css', array('parent-style'));
+    wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
 add_action('wp_enqueue_scripts', 'child_theme_styles');
